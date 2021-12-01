5455ef5902bc2ccc468b4c01d7ab05080445aca1
Shailja Sengar <65380554+shailja-singh@users.noreply.github.com>
2021-12-01 19:13:24 +0530

    Add files via upload

---
+++ build/precache-manifest.8f1f24c25255aefe34bb69a7ff402808.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "447f2148b1b23c8c83e9935efe467e0e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "edc15d0128836f967a28",
+    "url": "/static/css/main.37448acd.chunk.css"
+  },
+  {
+    "revision": "6fb81d14d21344781afc",
+    "url": "/static/js/2.303f87da.chunk.js"
+  },
+  {
+    "revision": "2182b49f32ec797f3b9c375cbe5c7fb3",
+    "url": "/static/js/2.303f87da.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "edc15d0128836f967a28",
+    "url": "/static/js/main.0993c985.chunk.js"
+  },
+  {
+    "revision": "45d009624ee73ac83b54",
+    "url": "/static/js/runtime-main.cdd681a7.js"
+  }
+]);
